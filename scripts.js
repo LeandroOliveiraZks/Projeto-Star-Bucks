0c84c0d2a554463f358e7ed437546675d23bb484
Leandro Oliveira <125764728+LeandroOliveiraZks@users.noreply.github.com>
2023-04-04 16:09:52 -0300

    enviando para o github projeto starbucks

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector (".circulo-color")
+
+function mudaImagem (mudaimagens) {
+    imagem.src = mudaimagens
+}
+
+function mudaCor (mudacores) {
+    circulo.style.background = mudacores
+}
